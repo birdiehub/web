c5afd942cf6c38d547907e5f3b282b6eb40f11b0
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-21 09:06:35 +0200

    created/modified user stuff (models, tables, seeders, services)

---
+++ database/migrations/2014_10_12_000010_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('zip');
+            $table->unsignedBigInteger('country');
+            $table->unsignedBigInteger('role');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->foreign('role')->references('id')->on('user_roles')->onDelete('cascade');
+            $table->foreign('country')->references('id')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
