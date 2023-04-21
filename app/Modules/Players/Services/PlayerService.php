0aa46d4fa963f392123d3c0718e1265ada6854cb
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-21 09:07:06 +0200

    created/modified player stuff

---
+++ app/Modules/Players/Services/PlayerService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Modules\Players\Services;
+
+use App\Modules\Core\Services\ServiceLanguages;
+
+class PlayerService extends ServiceLanguages
+{
+    protected $_rules = [
+
+    ];
+}
