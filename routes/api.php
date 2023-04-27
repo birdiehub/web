c4bac0cb1b9a5ffff44b6bb1064db8813d7f6142
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-27 20:17:12 +0200

    added authn users and country routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CountryApiController;
 use App\Http\Controllers\UserApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +21,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/auth/register', [AuthController::class, "register"]);
+Route::post('/auth/login', [AuthController::class, "login"]);
+
 Route::get('/users', [UserApiController::class, 'all']);
+Route::get('/users/{id}', [UserApiController::class, 'get'])->where('id', '[0-9]+');
+Route::put('/users/{id}', [UserApiController::class, 'update'])->where('id', '[0-9]+');
+Route::delete('/users/{id}', [UserApiController::class, 'delete'])->where('id', '[0-9]+');
+
+
+Route::get('/countries', [CountryApiController::class, 'all']);
+Route::get('/countries/list', [CountryApiController::class, 'list']);
+Route::post('/countries', [CountryApiController::class, 'create']);
+Route::get('/countries/{id}', [CountryApiController::class, 'get'])->where('id', '[0-9]+');
+Route::put('/countries/{id}', [CountryApiController::class, 'update'])->where('id', '[0-9]+');
+Route::delete('/countries/{id}', [CountryApiController::class, 'delete'])->where('id', '[0-9]+');
