f66305735ab1fbf219d2c036ac41948a2253b922
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-27 20:14:55 +0200

    added users relationships

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserRole extends Model
 {
@@ -12,5 +13,10 @@ class UserRole extends Model
     protected $table = 'user_roles';
 
     protected $guarded = ['*'];
+
+    public function users() : HasMany
+    {
+        return $this->hasMany(User::class, 'role', 'id');
+    }
 
 }
