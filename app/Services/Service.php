16f599af76f4254a37ee1bfa1fa1d64ebb8bba22
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-29 12:56:20 +0200

    refactored

---
+++ app/Services/Service.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Modules\Core\Validators\Validator;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class Service
+{
+    private Model $_model;
+
+    public function __construct(Model $model)
+    {
+        $this->_model = $model;
+    }
+
+    public function model() : Model
+    {
+        return $this->_model;
+    }
+
+    public function find($id) : Model
+    {
+        return $this->model()->findOrFail($id);
+    }
+
+    public function create($data) : Model
+    {
+        return $this->model()->create($data);
+    }
+
+    public function update($id, $data) : Model
+    {
+        $model = $this->find($id);
+        $model->update($data);
+        return $model;
+    }
+
+    public function delete($id) : bool {
+        $model = $this->find($id);
+        return $model->delete();
+    }
+
+}
