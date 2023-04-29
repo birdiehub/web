33fa871d6b200d9bc64496555b2b75c3cda8f7e3
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-29 16:46:27 +0200

    removed fedex cup standings from api

--- database/migrations/2014_10_12_000002_create_country_tables.php
+++ database/migrations/2014_10_12_000002_create_country_tables.php
@@ -6,37 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('code', 2)->unique();
+            $table->json('name'); // translatable
             $table->timestamps();
-        });
-
-        Schema::create('countries_language', function (Blueprint $table) {
-            $table->unsignedBigInteger('country_id');
-            $table->string('language', 10);
-            $table->string('name', 50);
-            $table->timestamps();
-            $table->primary(['country_id', 'language']);
-            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('countries_language');
         Schema::dropIfExists('countries');
     }
 };
