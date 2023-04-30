21cac7cc8221378a007de90988a0971acb4d7c57
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-30 22:07:15 +0200

    authorize roles

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -11,44 +11,59 @@ use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
+
+    private string $_class;
     private RoleService $_service;
     public function __construct(RoleService $service)
     {
         $this->_service = $service;
+        $this->_class = $this->_service->model()::class;
     }
 
     public function list() : RoleCollection
     {
+        $this->authorize("viewRoles", $this->_class);
+
         $roles = $this->_service->model()->get();
         return new RoleCollection($roles);
     }
 
     public function create(Request $request) : RoleResource
     {
+        $this->authorize("createRole", $this->_class);
+
         $role = $this->_service->create($request->all());
         return new RoleResource($role);
     }
 
     public function get($name) : RoleResource
     {
+        $this->authorize("viewRole", $this->_class);
+
         $role = $this->_service->find($name);
         return new RoleResource($role);
     }
 
     public function delete($name) : JsonResponse
     {
+        $this->authorize("deleteRole", $this->_service->find($name));
+
         $this->_service->delete($name);
         return Response::ok();
     }
 
     public function grantPermission($roleName, $permissionName) : JsonResponse
     {
+        $this->authorize("grantRolePermission", $this->_service->find($roleName));
+
         $this->_service->grantPermission($roleName, $permissionName);
         return Response::ok();
     }
 
     public function revokePermission($roleName, $permissionName) : JsonResponse
     {
+        $this->authorize("revokeRolePermission", $this->_service->find($roleName));
+
         $this->_service->revokePermission($roleName, $permissionName);
         return Response::ok();
     }
--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -2,22 +2,48 @@
 
 namespace App\Policies;
 
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Spatie\Permission\Models\Role;
 
 class RolePolicy
 {
 
-    // TODO: check per super-admin role, this can't be modified or deleted
-
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function viewRoles(User $user): bool
     {
-        //
+        return $user->hasPermissionTo('view-roles-list');
     }
+
+    public function viewRole(User $user): bool
+    {
+        return $user->hasPermissionTo('view-roles-details');
+    }
+
+
+    public function deleteRole(User $user, Role $role): bool
+    {
+        if ($role->name === 'super-admin') return false;
+        return $user->hasPermissionTo('delete-roles');
+    }
+
+
+    public function createRole(User $user): bool
+    {
+        return $user->hasPermissionTo('create-roles');
+    }
+
+    public function grantRolePermission(User $user, Role $role): bool
+    {
+        if ($role->name === 'super-admin') return false;
+        return $user->hasPermissionTo('grant-role-permissions');
+    }
+
+    public function revokeRolePermission(User $user, Role $role): bool
+    {
+        if ($role->name === 'super-admin') return false;
+        return $user->hasPermissionTo('revoke-role-permissions');
+    }
+
 }
