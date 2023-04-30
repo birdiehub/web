50bebf5e6802abefa01a9c99da4eae2b9a3c0e44
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-30 11:54:45 +0200

    removed translate trait and added to Resource class

--- app/Http/Resources/Resource.php
+++ app/Http/Resources/Resource.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 abstract class Resource extends JsonResource implements IResource
 {
-    use TranslateAttributes;
 
     protected string | null $_language;
     public function __construct($resource, $language = null)
@@ -15,4 +14,11 @@ abstract class Resource extends JsonResource implements IResource
         $this->_language = $language;
     }
 
+    protected function translate(string $attribute, bool $useFallbackLocale = true): string | null
+    {
+        $translation = $this->resource->getTranslation($attribute, $this->_language ?? "", $useFallbackLocale);
+        return $translation === "" ? null : $translation;
+    }
+
 }
+
--- app/Http/Resources/ResourceCollection.php
+++ app/Http/Resources/ResourceCollection.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Resources\Json\ResourceCollection as JsonResourceCollection;
 
 abstract class ResourceCollection extends JsonResourceCollection implements IResource
 {
-    use TranslateAttributes;
 
     protected string | null $_language;
     public function __construct($resource, $language = null)
