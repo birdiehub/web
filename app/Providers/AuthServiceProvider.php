45cb9c8dbafccc879f4d0dfad96deb8f1f613970
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-30 12:24:38 +0200

    moved user policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use App\Models\User;
-use App\Policies\UserPolicy;
+use App\Policies\Users\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
