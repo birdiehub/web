c39b806940aba65f3bcb9c8975cc11a750aba20e
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-30 11:53:59 +0200

    created access control policies, added todos for future reference

---
+++ app/Policies/AccessControl/PermissionPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Policies\AccessControl;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PermissionPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Policies/AccessControl/RolePolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Policies\AccessControl;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RolePolicy
+{
+
+    // TODO: check per super-admin role, this can't be modified or deleted
+
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+}
