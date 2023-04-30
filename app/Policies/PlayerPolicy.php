ba9eab9e29e69e38d72c36b4ff7da1d6dd9f1f24
thibo.verbeerst <contact@thiboverbeerst.com>
2023-04-30 21:42:05 +0200

    created player policies, but they are not implemented yet

--- app/Policies/PlayerPolicy.php
+++ app/Policies/PlayerPolicy.php
@@ -9,13 +9,31 @@ class PlayerPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function viewPlayers(User $user): bool
     {
-        //
+        return $user->hasPermissionTo('view-players-list');
     }
+
+    public function viewPlayer(User $user): bool
+    {
+        return $user->hasPermissionTo('view-players-details');
+    }
+
+    public function editPlayer(User $user): bool
+    {
+        return $user->hasPermissionTo('edit-players');
+    }
+
+    public function deletePlayer(User $user): bool
+    {
+        return $user->hasPermissionTo('delete-players');
+    }
+
+
+    public function createPlayer(User $user): bool
+    {
+        return $user->hasPermissionTo('create-players');
+    }
+
+
 }
