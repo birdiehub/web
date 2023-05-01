2ae91f99eeda80e0075a5af008fc9961d6f8bc4b
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-01 14:59:42 +0200

    changed email to gebruikersnaam

--- lang/nl/auth.php
+++ lang/nl/auth.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'failed'   => 'Deze combinatie van e-mailadres en wachtwoord is niet geldig.',
+    'failed'   => 'Deze combinatie van gebruikersnaam en wachtwoord is niet geldig.',
     'password' => 'Het opgegeven wachtwoord is onjuist.',
     'throttle' => 'Te veel mislukte aanmeldpogingen. Probeer het nog eens over :seconds seconden.',
 ];
