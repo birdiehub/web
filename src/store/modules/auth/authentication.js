2077b5ca36d2e38d7b74a96235d201bab573793a
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-18 14:25:36 +0200

    added auth requests

--- src/store/modules/auth/authentication.js
+++ src/store/modules/auth/authentication.js
@@ -14,6 +14,18 @@ const actions = {
     async login({ commit }, credentials) {
         await post(`auth/login`, credentials,(json) =>  saveToStorage('token', json.token));
     },
+    async register({ commit }, user) {
+        await post(`auth/register`, user,(json) =>  saveToStorage('token', json.token));
+    },
+    async logout({ commit }) {
+        await post(`auth/logout`,() =>  saveToStorage('token', undefined));
+    },
+    async refreshAuthentication({ commit }) {
+        await post(`auth/refresh`,(json) =>  saveToStorage('token', json.token));
+    },
+    async isAuthenticated({ commit }) {
+        return get(`auth/validate`,() =>  true, () =>  false);
+    },
     async fetchMe({ commit }) {
         await get(`auth/me`,(json) => commit('setMe', json.data));
     }
