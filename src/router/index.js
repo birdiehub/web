c149e930409fc568ae6818e2d9a9688f6bcf6f18
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-18 16:17:04 +0200

    added route guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,25 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import store from "@/store";
 
 import DashboardView from "@/views/App/DashboardView.vue";
 import AppLayout from "@/layouts/AppLayout.vue";
 import AuthenticationLayout from "@/layouts/AuthenticationLayout.vue";
 import LoginView from "@/views/Authentication/LoginView.vue";
 import RegisterView from "@/views/Authentication/RegisterView.vue";
+
+async function routeGuard(to, from, next) {
+  await store.dispatch('isAuthenticated').then((isAuthenticated) => {
+    if (to.meta.protected && !isAuthenticated) {
+      next('/auth/login');
+    }
+    else if (to.path.startsWith('/auth/') && isAuthenticated) {
+      next(from.path.startsWith('/auth/') ? '/dashboard' : from.path);
+    }
+    else {
+      next();
+    }
+  });
+}
 
 const routes = [
   {
@@ -19,6 +34,9 @@ const routes = [
         name: 'Dashboard',
         components: {
           App: DashboardView
+        },
+        meta: {
+          protected: true
         }
       }
     ]
@@ -35,6 +53,9 @@ const routes = [
         name: 'Login',
         components: {
             Authentication: LoginView
+        },
+        meta: {
+          protected: false
         }
       },
       {
@@ -42,6 +63,9 @@ const routes = [
         name: 'Register',
         components: {
           Authentication: RegisterView
+        },
+        meta: {
+          protected: false
         }
       }
     ]
@@ -52,5 +76,7 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 });
+
+router.beforeEach(routeGuard);
 
 export default router;
