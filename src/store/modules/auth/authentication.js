186f6514f23c439202a0ce24e18fdd786102521a
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-19 13:42:45 +0200

    push to route on login or register and return value of isAuthenticated

--- src/store/modules/auth/authentication.js
+++ src/store/modules/auth/authentication.js
@@ -1,5 +1,7 @@
 import { post, get } from "@/assets/js/data-connector/api-communication-abstractor";
 import { saveToStorage} from "@/assets/js/data-connector/local-storage-abstractor";
+import router from '@/router';
+
 
 const state = {
     me: undefined
@@ -11,12 +13,15 @@ const getters = {
 
 const actions = {
     async login({ commit }, credentials) {
-        await post(`auth/login`, credentials,(json) =>  saveToStorage('token', json.token));
+        await post(`auth/login`, credentials,(json) =>  {
+            saveToStorage('token', json.token);
+            router.push({ name: 'Dashboard' });
+        });
     },
     async register({ commit }, user) {
         await post(`auth/register`, user,(json) => {
             saveToStorage('token', json.token);
-            this.$router.push('/dashboard');
+            router.push({ name: 'Dashboard' });
         });
     },
     async logout({ commit }) {
@@ -26,7 +31,7 @@ const actions = {
         await post(`auth/refresh`, {}, (json) =>  saveToStorage('token', json.token));
     },
     async isAuthenticated({ commit }) {
-        await get(`auth/validate`,() =>  true, () =>  false);
+        return get(`auth/validate`,() =>  true, () =>  false);
     },
     async fetchMe({ commit }) {
         await get(`auth/me`,(json) => commit('setMe', json.data));
