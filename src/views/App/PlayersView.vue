0a2e55198b53827b15d4596b722acf57a9137dd6
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-20 00:55:24 +0200

    added logic to load players and added pageBar

--- src/views/App/PlayersView.vue
+++ src/views/App/PlayersView.vue
@@ -1,25 +1,64 @@
 <template>
     <HeaderContent :title="this.$translator.translate('app.views.players.title')"/>
-    <main class="main-content">
-
+    <load v-if="!loaded"/>
+    <main v-if="loaded" class="main-content">
+        <PageBar :lastPage="this.meta.last_page" :currentPage="this.meta.current_page" @changePage="(newPage) => {this.page = newPage}"/>
     </main>
 </template>
 
 <script>
 import HeaderContent from "@/components/Header/HeaderContent.vue";
-import Icon from "@/components/Icon/Icon.vue";
-import {mapGetters} from "vuex";
-import router from "@/router";
-import TextTile from "@/components/Tile/TextTile.vue";
-import ShortcutTile from "@/components/Tile/ShortcutTile.vue";
-import NotificationItem from "@/components/Notification/NotificationItem.vue";
+import {mapActions} from "vuex";
+import Load from "@/components/Load/Load.vue";
+import PageBar from "@/components/Pages/PageBar.vue";
 
 export default {
     name: "PlayersView",
     components: {
+        PageBar,
+        Load,
         HeaderContent
     },
     methods: {
+        ...mapActions(['fetchPlayers']),
+        async loadPlayers() {
+            this.loaded = false;
+            await this.fetchPlayers(this.page, this.sort, this.pageSize).then((json) => {
+                this.players = json.data;
+                this.meta = json.meta;
+                this.loaded = true;
+            });
+        }
+    },
+    async created() {
+        await this.loadPlayers();
+    },
+    data() {
+        return {
+            players: [],
+            meta: undefined,
+            loaded: false,
+            page: 1,
+            sort: "rank,asc",
+            pageSize: 20
+        }
+    },
+    watch: {
+        page: {
+            handler: async function (value) {
+                await this.loadPlayers();
+            }
+        },
+        sort: {
+            handler: async function (value) {
+                await this.loadPlayers();
+            }
+        },
+        pageSize: {
+            handler: async function (value) {
+                await this.loadPlayers();
+            }
+        }
     }
 };
 </script>
