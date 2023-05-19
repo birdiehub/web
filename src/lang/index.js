9073dd823c7f721d2d46e1ed72be9a7e5990747f
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-19 17:59:02 +0200

    added Translator class to translate web text

---
+++ src/lang/index.js
@@ -0,0 +1,14 @@
+import { createTranslator } from "@/lang/translator";
+
+import auth_en from "@/lang/translations/en/auth";
+import app_en from "@/lang/translations/en/app";
+
+
+const translator = createTranslator({
+    "en": {
+        auth: auth_en,
+        app: app_en
+    }
+});
+
+export default translator;
