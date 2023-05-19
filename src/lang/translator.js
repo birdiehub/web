d17d138e558329a35e1acc01e30ff54dbc893860
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-19 20:23:17 +0200

    made translator more advanced and optimized

--- src/lang/translator.js
+++ src/lang/translator.js
@@ -1,16 +1,56 @@
-import {loadFromStorage} from "@/assets/js/data-connector/local-storage-abstractor";
+import {loadFromStorage, saveToStorage} from "@/assets/js/data-connector/local-storage-abstractor";
 
 class Translator {
-    constructor(languages){
-        this.languages = languages;
-    }
-    translate(key){
-        return this._getProperty(this._language(), key.split('.'));
+    constructor(translations){
+        this.translations = translations;
+        this.clientLanguages = Object.keys(translations);
+        fetch('http://homestead.golf/api/languages')
+            .then(response => response.json())
+            .then(json => {
+                this.serverLanguages = json.data;
+            });
     }
 
-    _language() {
-        const lang = loadFromStorage('language');
-        return this.languages[lang];
+    languages() {
+        // return languages that are supported by both client and server
+        return this.serverLanguages.filter(lang => this.clientLanguages.includes(lang));
+    }
+
+    translate(key){
+        return this._getProperty(this.translations[this.language()], key.split('.'));
+    }
+
+    language() {
+        let lang = loadFromStorage('language');
+        if (lang && this.isLanguageSupported(lang))  return lang;
+
+        // fallback
+        lang = this.fallbackLanguage();
+        this.setLanguage(lang);
+        return lang;
+    }
+    setLanguage(lang) {
+        if (this.isLanguageSupported(lang)) {
+            saveToStorage('language', lang);
+        }
+    }
+
+    fallbackLanguage() {
+        // (1) return browser language if supported
+        // (2) return english if supported
+        // (3) return first language in list
+        let lang = this.browserLanguage();
+        if (this.isLanguageSupported(lang)) return lang;
+        return this.languages().includes('en') ? 'en' : this.languages()[0];
+    }
+
+    browserLanguage() {
+        const browserLanguage = navigator.language;
+        return browserLanguage.split('-')[0];
+    }
+
+    isLanguageSupported(lang) {
+        return this.languages().includes(lang);
     }
 
     _getProperty(obj, keys) {
@@ -24,6 +64,6 @@ class Translator {
     }
 }
 
-export function createTranslator(languages) {
-    return new Translator(languages);
+export function createTranslator(translations) {
+    return new Translator(translations);
 }
