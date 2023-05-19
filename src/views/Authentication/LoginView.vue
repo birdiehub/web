bd391f51af381e899e55d176d49cbc5a6b3afb87
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-19 13:43:17 +0200

    refactored

--- src/views/Authentication/LoginView.vue
+++ src/views/Authentication/LoginView.vue
@@ -3,12 +3,13 @@
       <h1>Use your Birdie account</h1>
       <div class="form-fields">
           <label for="username">Username</label>
-          <input v-model="username" type="text" id="username" name="username" required autocomplete="off" placeholder="Enter username"/>
+          <input v-model="credentials.username" type="text" id="username" name="username" required autocomplete="off" placeholder="Enter username"/>
           <label for="password">Password</label>
-          <input v-model="password" type="password" id="password" name="password" required autocomplete="off" placeholder="Enter password"/>
+          <input v-model="credentials.password" type="password" id="password" name="password" required autocomplete="off" placeholder="Enter password"/>
       </div>
       <div class="bottom-buttons">
           <TextIconButton :content="`Login`" :icon="`login`" :width="`8.5rem`" :height="`2rem`" :flexDirection="`row-reverse`" @click="clickedLogin"/>
+          <a href="#/auth/register">Create account.</a>
       </div>
   </form>
 </template>
@@ -25,16 +26,15 @@ export default {
     methods: {
         ...mapActions(['login']),
         clickedLogin() {
-            this.login({ username: this.username, password: this.password })
-                .then(() => {
-                    this.$router.push('/dashboard');
-                });
+            this.login(this.credentials);
         }
     },
     data() {
         return {
-            username: undefined,
-            password: undefined
+            credentials: {
+                username: undefined,
+                password: undefined
+            }
         };
     }
 }
