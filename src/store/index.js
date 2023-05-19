8d92d071cd7b10948025e1d8577ccf14a5a71372
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-20 00:55:44 +0200

    action to fetch players

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import { createStore } from "vuex";
 // modules
 import authentication from "@/store/modules/auth/authentication";
 import countries from "@/store/modules/countries/countries";
+import players from "@/store/modules/players/players";
 
 import notification from "@/store/modules/notification";
 
@@ -11,6 +12,7 @@ const store = createStore({
     modules: {
         authentication,
         countries,
+        players,
         notification
     }
 });
