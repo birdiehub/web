146352058690b2232df6c0479dec3fc5099dd514
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-19 20:22:46 +0200

    created component to set app lang

---
+++ src/components/Language/SelectLanguage.vue
@@ -0,0 +1,36 @@
+<template>
+    <Dropdown :name="`Languages`" :options="languageOptions()" :select="defaultLanguage()" @select="handleSelectedLanguage"/>
+</template>
+
+<script>
+
+import Dropdown from "@/components/Form/Dropdown.vue";
+import translator from "@/lang";
+
+export default {
+    name: "SelectLanguage",
+    components: {Dropdown},
+    methods: {
+        languageOptions() {
+            return translator.languages().map((language) => {
+                return {
+                    value: language,
+                    label: language
+                }
+            });
+        },
+        defaultLanguage() {
+            const language = translator.language();
+            return this.languageOptions().find((option) => option.value === language);
+        },
+        handleSelectedLanguage(lang) {
+            translator.setLanguage(lang.value);
+        }
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
