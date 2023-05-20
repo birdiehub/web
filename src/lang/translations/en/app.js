5e38c1725ef8a7572ce3140093f61d42ff5e48d1
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-20 04:01:28 +0200

    added lang (nl, en) files for app (temp, not finished)

--- src/lang/translations/en/app.js
+++ src/lang/translations/en/app.js
@@ -1,7 +1,39 @@
 
 export default {
+    "notifications": {
+        "welcome": "Welcome"
+    },
     "nav": {
         "dashboard": "Dashboard",
         "players": "Players"
     },
+    "views": {
+        "dashboard": {
+            "title": "Dashboard"
+        },
+        "players": {
+            "title": "Players",
+            "table": {
+                "rank": "Rank",
+                "name": "Name",
+                "headshot": "Headshot",
+                "country": "Country"
+            },
+            "filter": {
+                "sort": "Sort",
+                "sort_options": {
+                    "rank_f_to_l": "Rank: First to Last",
+                    "rank_l_to_f": "Rank: Last to First",
+                    "first_name_a_to_z": "First Name: A to Z",
+                    "first_name_z_to_a": "First Name: Z to A",
+                    "last_name_a_to_z": "Last Name: A to Z",
+                    "last_name_z_to_a": "Last Name: Z to A"
+                },
+                "show": "Show",
+                "show_records": "Records"
+            }
+        }
+    },
+    "next": "Next",
+    "previous": "Previous",
 };
---
+++ src/lang/translations/nl/app.js
@@ -0,0 +1,40 @@
+
+
+export default {
+    "notifications": {
+        "welcome": "Welkom"
+    },
+    "nav": {
+        "dashboard": "Dashboard",
+        "players": "Spelers"
+    },
+    "views": {
+        "dashboard": {
+            "title": "Dashboard"
+        },
+        "players": {
+            "title": "Spelers",
+            "table": {
+                "rank": "Rank",
+                "name": "Naam",
+                "headshot": "Hoofdfoto",
+                "country": "Land"
+            },
+            "filter": {
+                "sort": "Sorteer",
+                "sort_options": {
+                    "rank_f_to_l": "Rank: Eerst tot Laatst",
+                    "rank_l_to_f": "Rank: Laatst tot Eerst",
+                    "first_name_a_to_z": "Voornaam: A tot Z",
+                    "first_name_z_to_a": "Voornaam: Z tot A",
+                    "last_name_a_to_z": "Achternaam: A tot Z",
+                    "last_name_z_to_a": "Achternaam: Z tot A"
+                },
+                "show": "Toon",
+                "show_records": "Rijen",
+            }
+        }
+    },
+    "next": "Volgende",
+    "previous": "Vorige",
+};
