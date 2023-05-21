8fbcf7dac34141bfc39188157071848dffb2f33f
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-21 11:37:16 +0200

    added error route for 401 failure to logout

--- src/assets/js/data-connector/api-communication-abstractor.js
+++ src/assets/js/data-connector/api-communication-abstractor.js
@@ -2,28 +2,28 @@ import { loadFromStorage } from "@/assets/js/data-connector/local-storage-abstra
 import { API } from '@/main.js';
 import store from '@/store/index.js';
 
-function get(uri, successHandler = logJson, failureHandler = errorNotification) {
+function get(uri, successHandler = logJson, failureHandler = errorHandler) {
     const options = constructOptions('get');
     const request = new Request(API + uri, options);
 
     return call(request, successHandler, failureHandler);
 }
 
-function post(uri, body, successHandler = logJson, failureHandler = errorNotification) {
+function post(uri, body, successHandler = logJson, failureHandler = errorHandler) {
     const options = constructOptions('POST', body);
     const request = new Request(API + uri, options);
 
     return call(request, successHandler, failureHandler);
 }
 
-function put(uri, body, successHandler = logJson, failureHandler = errorNotification) {
+function put(uri, body, successHandler = logJson, failureHandler = errorHandler) {
     const options = constructOptions('PUT', body);
     const request = new Request(API + uri, options);
 
     return call(request, successHandler, failureHandler);
 }
 
-function remove(uri, successHandler = logJson, failureHandler = errorNotification) {
+function remove(uri, successHandler = logJson, failureHandler = errorHandler) {
     const options = constructOptions('DELETE');
     const request = new Request(API + uri, options);
 
@@ -53,27 +53,38 @@ function logJson(json) {
     console.log(json);
 }
 
-function errorNotification(response) {
-   try {
-       response.json().then(error => {
-       if (typeof error.cause=== "string") {
-           store.dispatch('createNotification', {content: error.cause, type: `error`});
-       } else if (typeof error.cause === "object") {
-           for (const cause in error.cause) {
-               if (!Array.isArray(error.cause[cause])) {
-                   store.dispatch('createNotification', {content: error.cause[cause], type: `error`});
-                   continue;
-               }
-               for (const message of error.cause[cause]) {
-                   store.dispatch('createNotification', {content: message, type: `error`});
-               }
-           }
-       }
-    });
-   }
-   catch (e) {
+function errorHandler(response) {
+    try {
+        response.json().then(error => {
+            errorRoute(error)
+            errorNotification(error);
+        });
+    }
+    catch (e) {
 
-   }
+    }
+}
+
+function errorRoute(error) {
+    if (error.failure === 401) {
+        store.dispatch('logout');
+    }
+}
+
+function errorNotification(error) {
+    if (typeof error.cause === "string") {
+        store.dispatch('createNotification', {content: error.cause, type: `error`});
+    } else if (typeof error.cause === "object") {
+        for (const cause in error.cause) {
+            if (!Array.isArray(error.cause[cause])) {
+                store.dispatch('createNotification', {content: error.cause[cause], type: `error`});
+                continue;
+            }
+            for (const message of error.cause[cause]) {
+                store.dispatch('createNotification', {content: message, type: `error`});
+            }
+        }
+    }
 }
 
 function call(request, successHandler, errorHandler) {
