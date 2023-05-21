5c5edb714458351933034bb162154eb576727dd9
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-21 19:21:40 +0200

    return to players when player not found

--- src/store/modules/players/player.js
+++ src/store/modules/players/player.js
@@ -1,10 +1,14 @@
-import { get } from "@/assets/js/data-connector/api-communication-abstractor";
+import { get } from "@/assets/js/data-connector/http";
 import translator from "@/lang";
+import store from "@/store";
 
 const actions = {
     async fetchPlayer({ commit }, params) {
         const { id } = params;
-        return get(`players/${id}?language=${translator.language()}`, (json) => json.data);
+        return get(`players/${id}?language=${translator.language()}`, (json) => json.data, (error) => {
+            store.dispatch("createNotification", {'type': 'error', 'content': 'Player not found'});
+            throw error;
+        });
     }
 }
 
--- src/views/App/PlayerView.vue
+++ src/views/App/PlayerView.vue
@@ -1,14 +1,15 @@
 <template>
-    <HeaderContent :title="this.$translator.translate('app.views.player.title')"/>
     <Load v-if="!loaded"/>
+    <div v-if="loaded">
+        <HeaderContent :title="this.player.full_name"/>
+    </div>
     <main v-if="loaded" class="main-content">
-        <div class="player-info">
-            <div class="player-info__image">
-                <img :src="this.player?.headshot" alt="Player image">
-            </div>
-            <div class="player-info__details">
-                <h2>{{ this.player.full_name }}</h2>
-                <p>{{ this.player?.bio }}</p>
+        <div class="player-basic-info box flex-gap-row">
+            <img :src="this.player.headshot ?? 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'" :alt="this.player.full_name"/>
+            <div>
+                <div>
+                    <p>DOB: {{ this.player?.birthday }}</p>
+                </div>
             </div>
         </div>
     </main>
@@ -30,9 +31,13 @@ export default {
         ...mapActions(['fetchPlayer']),
         loadPlayer() {
             this.loaded = false;
+            const playerId = this.$route.params.id;
+            if (!playerId) return this.$router.push('/players');
             this.fetchPlayer({ id: this.$route.params.id }).then((player) => {
                 this.player = player;
                 this.loaded = true;
+            }).catch(() => {
+                this.$router.push('/players');
             });
         },
     },
@@ -60,5 +65,30 @@ export default {
 
 
 <style scoped lang="scss">
+
+.player-basic-info {
+    background-color: var(--color-secondary);
+    height: 30vh;
+
+    * {
+        color: var(--color-white);
+    }
+
+    img {
+        flex: 1 1 20%;
+        width: 100%;
+        height: 100%;
+        transform: scale(0.8);
+        border-radius: 50%;
+        margin: 0 auto;
+        display: block;
+    }
+
+    > div {
+        flex: 1 1 80%;
+        display: flex;
+    }
+
+}
 
 </style>
