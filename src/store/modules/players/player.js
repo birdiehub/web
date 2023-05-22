8c20216af7f34b082dd344cdcfa14b11cd961bfe
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-22 17:38:50 +0200

    added create player action

--- src/store/modules/players/player.js
+++ src/store/modules/players/player.js
@@ -1,6 +1,7 @@
-import { get } from "@/assets/js/data-connector/http";
+import {get, post} from "@/assets/js/data-connector/http";
 import translator from "@/lang";
 import store from "@/store";
+import router from "@/router";
 
 const actions = {
     async fetchPlayer({ commit }, params) {
@@ -9,7 +10,14 @@ const actions = {
             store.dispatch("createNotification", {'type': 'error', 'content': 'Player not found'});
             throw error;
         });
-    }
+    },
+    async createPlayer({ commit }, player) {
+        console.log(player);
+        return post(`players?language=${translator.language()}`, player,(json) => {
+            store.dispatch("createNotification", {'type': 'success', 'content': `Player ${json.data.first_name} ${json.data.last_name} created`});
+            router.push('/players');
+        });
+    },
 }
 
 export default {
