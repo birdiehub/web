f9ac76ca86bec26b5c425f9a24e504df7048e9b7
thibo.verbeerst <contact@thiboverbeerst.com>
2023-05-22 10:48:38 +0200

    refactored and (fetch permissions of user, me)

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { createStore } from "vuex";
 
 // modules
 import authentication from "@/store/modules/auth/authentication";
+import user from "@/store/modules/auth/user";
 import countries from "@/store/modules/countries/countries";
 import players from "@/store/modules/players/players";
 import player from "@/store/modules/players/player";
@@ -12,6 +13,7 @@ import notification from "@/store/modules/notification";
 const store = createStore({
     modules: {
         authentication,
+        user,
         countries,
         players,
         player,
