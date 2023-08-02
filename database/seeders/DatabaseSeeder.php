f4171c3c0b9789b84cac5c963f635a4294b2ebd9
thibo.verbeerst <verbeerst.thibo@icloud.com>
2023-08-02 18:39:59 +0200

    add seeder of tournament to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CountrySeeder::class,
             AccessControlSeeder::class,
-            GolfSeeder::class
+            GolfSeeder::class,
+            GolfTournamentSeeder::class
         ]);
     }
 }
