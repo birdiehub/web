233b448891a21b42c7fec44ec00db52e57ba7ca5
thibo.verbeerst <verbeerst.thibo@icloud.com>
2023-08-03 14:31:16 +0200

    debugged sec command

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,27 +28,27 @@ php artisan jwt:secret
 # set environment variables
 # - APP_URL
 if [ -n "$APP_URL" ]; then
-    sed -i "s/APP_URL=.*/APP_URL=$APP_URL/" ./.env
+    sed -i "s@APP_URL=.*@APP_URL=\"$APP_URL\"@" ./.env
 fi
 # - DB_HOST
 if [ -n "$DB_HOST" ]; then
-    sed -i "s/DB_HOST=.*/DB_HOST=$DB_HOST/" ./.env
+    sed -i "s@DB_HOST=.*@DB_HOST=\"$DB_HOST\"@" ./.env
 fi
 # - DB_PORT
 if [ -n "$DB_PORT" ]; then
-    sed -i "s/DB_PORT=.*/DB_PORT=$DB_PORT/" ./.env
+    sed -i "s@DB_PORT=.*@DB_PORT=\"$DB_PORT\"@" ./.env
 fi
 # - DB_DATABASE
 if [ -n "$DB_DATABASE" ]; then
-    sed -i "s/DB_DATABASE=.*/DB_DATABASE=$DB_DATABASE/" ./.env
+    sed -i "s@DB_DATABASE=.*@DB_DATABASE=\"$DB_DATABASE\"@" ./.env
 fi
 # - DB_USERNAME
 if [ -n "$DB_USERNAME" ]; then
-    sed -i "s/DB_USERNAME=.*/DB_USERNAME=$DB_USERNAME/" ./.env
+    sed -i "s@DB_USERNAME=.*@DB_USERNAME=\"$DB_USERNAME\"@" ./.env
 fi
 # - DB_PASSWORD
 if [ -n "$DB_PASSWORD" ]; then
-    sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$DB_PASSWORD/" ./.env
+    sed -i "s@DB_PASSWORD=.*@DB_PASSWORD=\"$DB_PASSWORD\"@" ./.env
 fi
 
 php artisan config:clear
